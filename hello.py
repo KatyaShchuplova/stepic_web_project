cac9bf0f2ac38c606842ac3e5c4e8186d0aa02d6
Katya Shchuplova <e.shchuplova@gmail.com>
2016-05-21 15:30:09 +0300

    Commit 5

---
+++ hello.py
@@ -0,0 +1,7 @@
+def wsgi_application(environ, start_response):
+    string = str(environ["QUERY_STRING"])
+    body = string.replace("&", "\n")
+    status = "200 OK"
+    headers = [("Content-Type", "text/plan")]
+    start_response(status, headers)
+    print(body)
